86ffa097f0384ae2c4db21b96d3c96d4e68dc219
Adrián Zúñiga <jaime.ziga@gmail.com>
2018-04-10 11:18:48 -0500

    Strong Password

--- strings/strong-password.js
+++ strings/strong-password.js
@@ -1,8 +1,8 @@
 process.stdin.resume()
 process.stdin.setEncoding('ascii')
 
-var input_stdin       = "";
-var input_stdin_array = "";
+var input_stdin       = '';
+var input_stdin_array = '';
 var input_currentline = 0;
 
 process.stdin.on('data', function (data) {
@@ -10,12 +10,12 @@ process.stdin.on('data', function (data) {
 })
 
 process.stdin.on('end', function () {
-	input_stdin_array = input_stdin.split("\n")
+	input_stdin_array = input_stdin.split('\n')
 	main()
 })
 
 process.on('SIGINT', function () {
-	input_stdin_array = input_stdin.split("\n")
+	input_stdin_array = input_stdin.split('\n')
 	main()
 	process.exit()
 })
@@ -26,18 +26,21 @@ function readLine() {
 
 function minimumNumber(n, password)
 {
-	let min_length         = 6;
-	let numbers            = "0123456789"
-	let lower_case         = "abcdefghijklmnopqrstuvwxyz"
-	let upper_case         = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	let special_characters = "!@#$%^&*()-+"
+	let min_length         = 6
+	let numbers            = '0123456789'
+	let lower_case         = 'abcdefghijklmnopqrstuvwxyz'
+	let upper_case         = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	let special_characters = '!@#$%^&*()-+'
 
 	if (password.length < min_length)
 		return min_length - password.length
 
-	let needs_numbers = numbers.split('').map(char => !password.includes(char))
+	let needs_numbers            = +(!numbers.split('').map(number => password.includes(number) || '').join(''))
+	let needs_lower_case         = +(!lower_case.split('').map(char => password.includes(char) || '').join(''))
+	let needs_upper_case         = +(!upper_case.split('').map(char => password.includes(char) || '').join(''))
+	let needs_special_characters = +(!special_characters.split('').map(char => password.includes(char) || '').join(''))
 
-	return needs_numbers
+	return needs_numbers + needs_lower_case + needs_upper_case + needs_special_characters
 }
 
 function main()
@@ -46,5 +49,7 @@ function main()
 	var password = readLine()
 	var answer   = minimumNumber(n, password)
 
-	process.stdout.write("" + answer + "\n")
+	console.log(password);
+
+	process.stdout.write(answer + '\n')
 }
